5577724ec9ec4c3d4c8d377610eab1190c22b9c6
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-18 16:12:17 +0100

    Add App component to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./components/App";
+import GlobalStyle from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
-  </React.StrictMode>
+    <BrowserRouter>
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
