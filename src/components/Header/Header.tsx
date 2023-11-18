454f2b216304029bf9966a7e8e4cff5ef0082063
Patricia <patricia.femenias@gmail.com>
2023-11-18 17:00:12 +0100

    Add HeaderStyled

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,18 @@
+import HeaderStyled from "./HeaderStyled";
+
 const Header = (): React.ReactElement => {
   return (
-    <header>
-      <div>
+    <HeaderStyled>
+      <div className="logo-container">
         <img
+          className="header-logo"
           src="/images/logo.webp"
           alt="Killer Robots logo"
           width="400"
           height="4302"
         />
       </div>
-    </header>
+    </HeaderStyled>
   );
 };
 
