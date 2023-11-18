a69433d4abae62f9280ba35dd1eaae49ba53b557
Patricia <patricia.femenias@gmail.com>
2023-11-18 20:03:47 +0100

    Include Header in App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from "react-router-dom";
 import AppStyled from "./App.styled";
+import Header from "./Header/Header";
 
 const App = (): React.ReactElement => {
   return (
     <AppStyled>
+      <Header />
       <main>
         <h2 className="header__title">Killer Robots</h2>
         <Routes>
