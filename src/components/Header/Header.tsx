d83e49c960c9073c487b217a93bb8b467ee2ed14
Patricia <patricia.femenias@gmail.com>
2023-11-18 16:21:06 +0100

    Add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+const Header = (): React.ReactElement => {
+  return (
+    <header>
+      <div>
+        <img
+          src="/images/logo.webp"
+          alt="Killer Robots logo"
+          width="400"
+          height="4302"
+        />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
