0faec61e48867d506a5c6ccd85ae4f18a0555d6b
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-18 19:35:08 +0100

    Add Homepage styles

---
+++ src/pages/Homepage/Homepage.styled.ts
@@ -0,0 +1,10 @@
+import { styled } from "styled-components";
+
+const HomepageStyled = styled.header`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default HomepageStyled;
