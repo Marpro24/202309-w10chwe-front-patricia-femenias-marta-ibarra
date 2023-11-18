01cda88ef570844cde2856b003c2da89ec5c2f40
Marta Ibarra <martaa.ibarra@hotmail.com>
2023-11-18 18:04:10 +0100

    Delete div tag

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,13 @@ import { BrowserRouter } from "react-router-dom";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
-import GlobalStyle from "./styles/GlobalStyles";
+import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-<<<<<<< HEAD
     <BrowserRouter>
       <GlobalStyle />
       <App />
     </BrowserRouter>
-=======
-    <div />
->>>>>>> main
   </React.StrictMode>,
 );
