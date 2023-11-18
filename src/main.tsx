2cb00790f13d27becf862fa302eab8b8f5e181fb
Patricia <patricia.femenias@gmail.com>
2023-11-18 23:14:23 +0100

    Add mainTheme

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
 import GlobalStyle from "./styles/GlobalStyle";
+import { ThemeProvider } from "styled-components";
+import mainTheme from "./styles/mainTheme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyle />
-      <App />
+      <ThemeProvider theme={mainTheme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    main: "#000",
+    accent: "#991e14",
+    light: "#fff",
+    degraded: "#e1281c, #c1b5b4, #e1281c",
+  },
+  typography: {
+    mainFontFamily: "Verdana, Geneva, Tahoma, sans-serif",
+  },
+};
+
+export default mainTheme;
